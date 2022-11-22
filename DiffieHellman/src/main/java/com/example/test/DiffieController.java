ed78a1797b530c7e3e5f11a9f1bcd0cf941dd83f
DanielWilliamFaessler <daniel.faesslerasmodi@gmail.com>
2022-11-22 11:18:34 +0100

    Quick and easy pseudo random number generator

--- DiffieHellman/src/main/java/com/example/test/DiffieController.java
+++ DiffieHellman/src/main/java/com/example/test/DiffieController.java
@@ -3,7 +3,10 @@ package com.example.test;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class DiffieController {
+    private int min = 17;
+    private int max = 200;
     @FXML
     private Label PrimeAText;
     @FXML
@@ -11,10 +14,10 @@ public class DiffieController {
 
     @FXML
     protected void onGenAButtonClick() {
-        PrimeAText.setText("Here comes Primenumber");
+        PrimeAText.setText("Here comes KeyA: "+ Math.floor(Math.random()*(max-min+1)+min));
     }
     @FXML
     protected void onGenBButtonClick() {
-        PrimeBText.setText("Here comes Primenumber");
+        PrimeBText.setText("Here comes KeyB: "+ Math.floor(Math.random()*(max-min+1)+min) );
     }
 }
